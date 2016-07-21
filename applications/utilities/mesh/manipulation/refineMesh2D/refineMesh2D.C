3b13291d4630eea6b603227bc884e1f11148082e
Jakub Pola <jakub.pola@gmail.com>
2016-07-21 14:38:41 +0200

    Created refineMesh2D program in applications/utilities/manipulation for code development use case tutorials/mesh/adaptiveLocalMeshRefinement2D/RubicksCube with this program for testing. There are several new classes which were created base on 3D case, they are dynamicRefineFvMesh2D, hexRef4 and refinementHistory2D currently they are doing exacly what the original code is doing I've just adopted the naming convention and code structure

---
+++ applications/utilities/mesh/manipulation/refineMesh2D/refineMesh2D.C
@@ -0,0 +1,86 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    refineMesh2D
+
+Description
+    Adaptive mesh refinement for 2D mesh
+
+\*---------------------------------------------------------------------------*/
+
+
+#include "argList.H"
+#include "Time.H"
+#include "dynamicFvMesh.H"
+
+using namespace Foam;
+
+int main(int argc, char* argv[])
+{
+    //I put the from #includes explicitly here for better code management.
+
+    //#include "setRootCase.H"
+    Foam::argList args(argc, argv);
+    if (!args.checkRootCase())
+    {
+        Foam::FatalError.exit();
+    }
+
+    //#include "createTime.H"
+    Foam::Info<< "Create time\n" << Foam::endl;
+    Foam::Time runTime(Foam::Time::controlDictName, args);
+
+    runTime.functionObjects().off();
+
+    //#include "createDynamicFvMesh.H"
+    Info<< "Create mesh for time = "
+        << runTime.timeName() << nl << endl;
+
+    autoPtr<dynamicFvMesh> meshPtr
+    (
+        dynamicFvMesh::New
+        (
+            IOobject
+            (
+                dynamicFvMesh::defaultRegion,
+                runTime.timeName(),
+                runTime,
+                IOobject::MUST_READ
+            )
+        )
+    );
+    Info << "\nLook here in the tutorial case RubiksCube/constant I defined dynamicMeshDict\n"
+         << "There in line #9. dynamicFvMesh dynamicRefineFvMesh2D caused that\n"
+         << "our mes is of type dynamicRefineFvMesh2D. Briliant! :)\n" << endl;
+
+    dynamicFvMesh& mesh = meshPtr();
+
+
+    //Now select candidate cells and call mesh.update() to do 2D refinement :P
+
+	
+	Info << "Refine Mesh 2D End\n" << endl;
+	return 0;
+
+}
